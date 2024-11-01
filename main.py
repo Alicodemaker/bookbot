f7b63b7eb1fc024dbf1ca1fc32a23e9cbbf85dd8
Alicodemaker <ali.gogo@live.dk>
2024-11-01 11:18:36 +0100

    added a word counter to the book reader script

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ def main():
     with open("books/frankenstein.txt") as f:
         text = f.read()
         print(text)
+        wordcounter = 0
+        words = text.split()
+        for i in words:
+            wordcounter += 1
+        print(wordcounter)
+    
 
 main()
